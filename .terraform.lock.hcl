9e1baeea8ed9f6b29b01aa5df5596946db4dd330
heiwa4126 <heiwa4126@gmail.com>
2022-07-21 13:23:18 +0900

    update

---
+++ .terraform.lock.hcl
@@ -0,0 +1,24 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/aws" {
+  version     = "4.22.0"
+  constraints = "~> 4.16"
+  hashes = [
+    "h1:RxPzK6VFHz6qZMZUVhE03j9Cf5CvnLr14egtq5yxD1E=",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/local" {
+  version = "2.2.3"
+  hashes = [
+    "h1:aWp5iSUxBGgPv1UnV5yag9Pb0N+U1I0sZb38AXBFO8A=",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/random" {
+  version = "3.3.2"
+  hashes = [
+    "h1:H5V+7iXol/EHB2+BUMzGlpIiCOdV74H8YjzCxnSAWcg=",
+  ]
+}
--- user.tf
+++ user.tf
@@ -1,19 +1,53 @@
+data "local_file" "my_pgp_pub_key" {
+  filename = "./pgp/my-public-key.gpg"
+}
+
 resource "aws_iam_user" "user1" {
   name = "${var.prefix}user1-${random_id.id.hex}"
   path = "/example/"
-
   tags = {
     project-name = "falcon"
   }
 }
 
-data "local_file" "my_pgp_pub_key" {
-  filename = "./pgp/my-public-key.gpg"
+resource "aws_iam_user_policy" "user1" {
+  user = aws_iam_user.user1.name
+  policy = data.aws_iam_policy_document.AllResourcesLambdaNoTags.json
 }
 
 resource "aws_iam_access_key" "user1" {
-  user = aws_iam_user.user1.name
+  user    = aws_iam_user.user1.name
   pgp_key = data.local_file.my_pgp_pub_key.content_base64
+}
+
+resource "aws_iam_user" "user2" {
+  name = "${var.prefix}user2-${random_id.id.hex}"
+  path = "/example/"
+  tags = {
+    project-name = "eagle"
+  }
+}
+
+resource "aws_iam_user_policy" "user2" {
+  user = aws_iam_user.user2.name
+  policy = data.aws_iam_policy_document.AllResourcesLambdaNoTags.json
+}
+
+resource "aws_iam_access_key" "user2" {
+  user    = aws_iam_user.user2.name
+  pgp_key = data.local_file.my_pgp_pub_key.content_base64
+}
+
+data "aws_iam_policy_document" "AllResourcesLambdaNoTags" {
+  statement {
+    sid    = "AllResourcesLambdaNoTags"
+    effect = "Allow"
+    actions = [
+      "lambda:ListFunctions",
+      "lambda:GetAccountSettings"
+    ]
+    resources = ["*"]
+  }
 }
 
 #----
@@ -22,4 +56,10 @@ output "user1_id" {
 }
 output "user1_secret" {
   value = aws_iam_access_key.user1.encrypted_secret
+}
+output "user2_id" {
+  value = aws_iam_access_key.user2.id
+}
+output "user2_secret" {
+  value = aws_iam_access_key.user2.encrypted_secret
 }
